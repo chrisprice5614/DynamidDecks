2b5cf50949a573df2c2996bc4441b27e3c475ade
chrisprice5614 <33729338+chrisprice5614@users.noreply.github.com>
2023-10-26 13:11:14 -0700

    Add files via upload

--- gallery.js
+++ gallery.js
@@ -1,9 +1,2 @@
 //For all the gallery stuff
 //Flood with this array of pictures
-var items = [
-    {
-        catagoryMajor: "",
-        catagoryMinor: "",
-        link: "/blog/august2023.html"
-    },
-]
