fe1cf8415d9756d3a5b8f0d0d7ab18b6629cb74e
chrisprice5614 <33729338+chrisprice5614@users.noreply.github.com>
2023-12-05 13:48:31 -0800

    Update blog.js

--- blog/blog.js
+++ blog/blog.js
@@ -3,7 +3,7 @@
 var items = [
     {
         title: "Deck, Patio, Porch. Which is it?",
-        date: new Date(2023, 12, 5, 12, 00),
+        date: new Date(2023, 12, 5, 12, 0),
         author: "Chris Price",
         text: "We've all heard these terms, but what's the difference between them?",
         img: "/blog/01/titlecard.jpg",
