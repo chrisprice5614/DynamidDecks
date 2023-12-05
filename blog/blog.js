6d6c156a3b8a9dd9c280daa529fdb2b4944c241f
chrisprice5614 <33729338+chrisprice5614@users.noreply.github.com>
2023-12-05 11:47:08 -0800

    Add files via upload

---
+++ blog/blog.js
@@ -0,0 +1,12 @@
+///id = YYYYMMDDX X = Number of article that day
+//Always add to the top
+var items = [
+    {
+        title: "Deck, Patio, Porch. Which is it?",
+        date: new Date(2023, 9, 18, 4, 21),
+        author: "Chris Price",
+        text: "We've all heard these terms, but what's the difference between them?",
+        img: "/blog/01/titlecard.jpg",
+        link: "/blog/01/post.html"
+    },
+]
