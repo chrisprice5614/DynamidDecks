669bc4dc2af9fdad533324e564a05c3530dd2305
chrisprice5614 <33729338+chrisprice5614@users.noreply.github.com>
2025-03-16 22:59:22 -0700

    Add files via upload

---
+++ blog.js
@@ -0,0 +1,19 @@
+///id = YYYYMMDDX X = Number of article that day
+//Always add to the top
+var items = [
+    {
+        title: "Benefits of Azek PVC Decking for Homeowners",
+        img: "/blog/03/01.jpg",
+        link: "/blog/03/blog.html"
+    },
+    {
+        title: "Deck, Patio, Porch. Which is it?",
+        img: "/blog/01/titlecard.jpg",
+        link: "/blog/01/post.html"
+    },
+    {
+        title: "Deck Furnishing Guide",
+        img: "/blog/02/titlecard.jpg",
+        link: "/blog/02/post.html"
+    }
+]
